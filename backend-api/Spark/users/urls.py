e24fc374ad0d79a4b209090cba923c16cfa295b4
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-15 19:37:49 -0300

    update user app

--- backend-api/Spark/users/urls.py
+++ backend-api/Spark/users/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('', UserListCreateView.as_view(), name='user-list-create'),
     path('<int:pk>/', UserDetailView.as_view(), name='user-detail'),
-    path('<int:pk>/validate-signature/', ValidateUserSignatureView.as_view(), name='validate-signature'),
+    path('validate-signature/', ValidateUserSignatureView.as_view(), name='validate-signature'),
     path("login/", CustomAuthToken.as_view()),
 
     # path('user/<str:wallet_id>/', CustomUserDetailView.as_view(), name='customuser-detail'),
--- backend-api/Spark/users/views.py
+++ backend-api/Spark/users/views.py
@@ -5,22 +5,32 @@ from .models import CustomUser
 from .serializer import CustomUserSerializer, ValidateUserSignatureRequestSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
-
+from rest_framework.permissions import IsAuthenticated
+import requests
 
 
 class UserListCreateView(generics.ListCreateAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
 
+    def perform_create(self, serializer):
+        user = self.request.user # Obtém o usuário autenticado
+        serializer.save(user=user)
+
 class UserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
+    permission_classes = [IsAuthenticated]  # Permissões necessárias
 
+    def perform_create(self, serializer):
+        user = self.request.user # Obtém o usuário autenticado
+        serializer.save(user=user)
 class ValidateUserSignatureView(generics.CreateAPIView):
     """
     View para validar a assinatura digital de um usuário e atualizar o campo user_validated.
     """
     serializer_class = ValidateUserSignatureRequestSerializer
+    permission_classes = [IsAuthenticated]  # Permissões necessárias
 
     def get_queryset(self):
         if getattr(self, 'swagger_fake_view', False):
@@ -28,9 +38,8 @@ class ValidateUserSignatureView(generics.CreateAPIView):
         return CustomUser.objects.all()
     @swagger_auto_schema(request_body=ValidateUserSignatureRequestSerializer)
     def post(self, request, *args, **kwargs):
-        user_id = kwargs.get('pk')
         try:
-            user = CustomUser.objects.get(pk=user_id)
+            user = CustomUser.objects.get(pk=self.request.user.id)
             serializer = self.get_serializer(data=request.data)
             serializer.is_valid(raise_exception=True)
             if self.validate_signature(user= user, signature=serializer.validated_data['signature']):
@@ -38,20 +47,44 @@ class ValidateUserSignatureView(generics.CreateAPIView):
                 user.save()
                 return Response({'message': 'User signature validated successfully.'}, status=status.HTTP_200_OK)
 
-            return Response({'message': 'Signature validation failed.'}, status=status.HTTP_400_BAD_REQUEST)
+            return Response({'message': f'Signature validation failed. For user {self.request.user.id}'}, status=status.HTTP_400_BAD_REQUEST)
 
         except CustomUser.DoesNotExist:
             return Response({'message': 'User not found.'}, status=status.HTTP_404_NOT_FOUND)
     
     def validate_signature(self, user, signature):
-        # Aqui você deve implementar a lógica de validação da assinatura digital com a Receita
-        # Retorne True se validado, caso contrário False
-        return True  # Substitua pela lógica real
+        url = "https://validar.iti.gov.br/url"
+
+        payload = {"url": f"{signature}"}
+        headers = {
+            "Accept": "/",
+            "Accept-Language": "pt-BR,pt;q=0.9,en-US;q=0.8,en;q=0.7",
+            "Connection": "keep-alive",
+            "Content-Type": "application/json",
+            "Origin": "https://validar.iti.gov.br/",
+            "Referer": "https://validar.iti.gov.br/",
+            "Sec-Fetch-Dest": "empty",
+            "Sec-Fetch-Mode": "cors",
+            "Sec-Fetch-Site": "same-origin",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36",
+            "sec-ch-ua": '"Not/A)Brand";v="8", "Chromium";v="126", "Google Chrome";v="126"',
+            "sec-ch-ua-mobile": "?0",
+            "sec-ch-ua-platform": "Windows"
+        }
+
+        response = requests.request("POST", url, json=payload, headers=headers)
+        return response.status_code != 200
     
 class CustomUserDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = CustomUser.objects.all()
     serializer_class = CustomUserSerializer
     lookup_field = 'wallet_id'
+
+    permission_classes = [IsAuthenticated]  # Permissões necessárias
+
+    def perform_create(self, serializer):
+        user = self.request.user # Obtém o usuário autenticado
+        serializer.save(user=user)
 
 
 class CustomAuthToken(ObtainAuthToken):
