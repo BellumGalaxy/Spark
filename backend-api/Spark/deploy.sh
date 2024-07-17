2ddd4c316194b1e8695e5a0870fbb6b93310447c
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-17 20:39:55 -0300

    update make migrations

--- backend-api/Spark/deploy.sh
+++ backend-api/Spark/deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
 # Aplica as migrações do Django
-python manage.py makemigrations
-python manage.py migrate
+python manage.py makemigrations 
+1
+1
+# python manage.py migrate
 
-# Inicia o servidor Django
-python manage.py runserver 0.0.0.0:8000
+# # Inicia o servidor Django
+# python manage.py runserver 0.0.0.0:8000
