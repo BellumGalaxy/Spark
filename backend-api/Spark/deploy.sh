5b811a7a2d295ed1e74a3b11ce5c64258e63511b
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-17 20:46:28 -0300

    restore deploy.sh

--- backend-api/Spark/deploy.sh
+++ backend-api/Spark/deploy.sh
@@ -2,9 +2,8 @@
 
 # Aplica as migrações do Django
 python manage.py makemigrations 
-1
-1
-# python manage.py migrate
+
+python manage.py migrate
 
 # # Inicia o servidor Django
-# python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000
