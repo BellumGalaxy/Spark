5f2f1675347cad80d2cfc0ced07eb8b085b3a0b4
Cayo Tarcisio <cayotarcisio51@gmail.com>
2024-07-17 20:23:38 -0300

    firts commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import './App.css';
 import Homepage from './pages/Homepage';
+import Conta from './components/Conta';
+import RegisterConta from './components/RegisterConta';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 
 function App() {
 
   return (
     <>
-     <Homepage />
+     <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<RegisterConta />} />
+          <Route path="/conta" element={<Conta />} />
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+
+  );
 }
 
 export default App
