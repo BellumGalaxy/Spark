c5a871790bdf19b368d792d4aa59472657707486
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-19 08:45:47 -0300

    fix: cors

--- backend-api/Spark/Spark/settings.py
+++ backend-api/Spark/Spark/settings.py
@@ -53,12 +53,14 @@ PROJECT_APP = [
 THIRD_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
+    'corsheaders',
     'drf_yasg'
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APP + THIRD_APPS
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,7 +69,13 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://142.93.189.23:8000",
+    "https://seu-dominio.com",
+]
 ROOT_URLCONF = 'Spark.urls'
 
 TEMPLATES = [
@@ -102,6 +110,7 @@ DATABASES = {
         'PORT': os.environ.get('DB_PORT'),
     }
 }
+
 
 
 # Password validation
