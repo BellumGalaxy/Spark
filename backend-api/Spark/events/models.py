6bebad27932c75b5a776d9f9a91c01ad9e67c824
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-19 18:16:16 -0300

    create new models infos

--- backend-api/Spark/events/models.py
+++ backend-api/Spark/events/models.py
@@ -4,6 +4,8 @@ class Event(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
     location = models.CharField(max_length=100)
+    amount_total = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_actual = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(CustomUser, related_name='received_event', on_delete=models.CASCADE, limit_choices_to={'type_user': 'athlete'},default=1)
 
     def __str__(self):
