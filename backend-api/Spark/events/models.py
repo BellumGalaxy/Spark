4ed9959386c6eb7839c77b4a3a66a3c85c74157b
Gabriel Schneider <gabrielmunizsch@gmail.com>
2024-07-19 18:17:13 -0300

    fix: default value

--- backend-api/Spark/events/models.py
+++ backend-api/Spark/events/models.py
@@ -4,7 +4,7 @@ class Event(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateField()
     location = models.CharField(max_length=100)
-    amount_total = models.DecimalField(max_digits=10, decimal_places=2)
+    amount_total = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
     amount_actual = models.DecimalField(max_digits=10, decimal_places=2)
     user = models.ForeignKey(CustomUser, related_name='received_event', on_delete=models.CASCADE, limit_choices_to={'type_user': 'athlete'},default=1)
 
